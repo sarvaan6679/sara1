30b191b36bec8d63fd121a86266ba4c34ac84182
sarvaan6679 <notorious6679@gmail.com>
2022-03-03 17:53:37 +0530

    karthi updated his code

--- src/org/sar/Execute.java
+++ src/org/sar/Execute.java
@@ -4,8 +4,8 @@ public class Execute {
 	public static void main(String[] args) {
 		System.out.println("vicky");
 		System.out.println("vedha");
-		System.out.println("9994125633");
-		System.out.println("9985698565");
+		System.out.println("tamizhisai");
+		System.out.println("tamilnadu");
 	}
 
 }
